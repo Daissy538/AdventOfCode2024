471610ee5e7a7e5d8a0528e1bd4c75364f7c315d
hilde.hamer <hilde.hamer@justeattakeaway.com>
2024-12-06 15:25:21 +0100

    Solved puzzle 6 part 1

---
+++ src/puzzles/puzzle_6.ts
@@ -0,0 +1,117 @@
+import {Puzzle} from "./base/puzzleBase.ts";
+
+export class Puzzle6 extends Puzzle {
+    protected solvePartOne(input: string): number {
+        const map = this.readLines(input);
+
+        let guardCoordinates = this.findGuardCoordinates(map);
+        const visitedItems: Set<string> = new Set<string>();
+        visitedItems.add(guardCoordinates[0]+"+"+guardCoordinates[1]);
+
+        let direction = map[guardCoordinates[0]][guardCoordinates[1]];
+
+        let walking = true
+        while(walking){
+
+            switch (direction){
+                case "^":
+                    if(guardCoordinates[0]-1 === -1){
+                        walking = false;
+                        break;
+                    }
+                    const up = map[guardCoordinates[0]-1][guardCoordinates[1]];
+                    if(up === "#"){
+                        direction = ">";
+                    }
+                    else{
+                        guardCoordinates = [guardCoordinates[0]-1,guardCoordinates[1]];
+                        visitedItems.add(guardCoordinates[0]+"+"+guardCoordinates[1]);
+                    }
+                    break;
+                case ">":
+                    if(guardCoordinates[1]+1 === map[guardCoordinates[0]].length){
+                        walking = false;
+                        break;
+                    }
+                    const right = map[guardCoordinates[0]][guardCoordinates[1]+1];
+
+                    if(right === "#"){
+                        direction = "v";
+                    }
+                    else{
+                        guardCoordinates = [guardCoordinates[0],guardCoordinates[1]+1];
+                        visitedItems.add(guardCoordinates[0]+"+"+guardCoordinates[1]);
+                    }
+                    break;
+                case "<":
+                    if(guardCoordinates[1]-1 === -1){
+                        walking = false;
+                        break;
+                    }
+                    const left = map[guardCoordinates[0]][guardCoordinates[1]-1];
+
+                    if(left === "#"){
+                        direction = "^";
+                    }
+                    else{
+                        guardCoordinates = [guardCoordinates[0],guardCoordinates[1]-1];
+                        visitedItems.add(guardCoordinates[0]+"+"+guardCoordinates[1]);
+                    }
+                    break;
+                case "v":
+                    if(guardCoordinates[0]+1 === map.length){
+                        walking = false;
+                        break;
+                    }
+
+                    const down = map[guardCoordinates[0]+1][guardCoordinates[1]];
+                    if(down === "#"){
+                        direction = "<";
+                    }
+                    else{
+                        guardCoordinates = [guardCoordinates[0]+1,guardCoordinates[1]];
+
+                        visitedItems.add(guardCoordinates[0]+"+"+guardCoordinates[1]);
+                    }
+                    break;
+            }
+        }
+
+        return visitedItems.size;
+    }
+
+
+
+    protected solvePartTwo(input: string): number {
+        return 0;
+    }
+
+    private findGuardCoordinates(map: string[][]): [number, number]{
+        let response: [number, number] = [0,0];
+
+        for(let row = 0; row < map.length; row++){
+            for(let col = 0; col < map[row].length; col++){
+                if(map[row][col] === "^" || map[row][col] === "v" || map[row][col] === ">" || map[row][col] === "<"){
+                    response = [row, col];
+                    return response;
+                }
+            }
+        }
+
+        throw new Error("No guard found");
+    }
+
+    private readLines(input: string): string[][] {
+        const response: string[][] = [];
+        const lines = input.split("\n");
+
+        lines.forEach((line) => {
+            const c = line.split("");
+            response.push(c);
+        });
+
+        return response;
+
+    }
+
+}
--- src/tests/puzzle.spec.ts
+++ src/tests/puzzle.spec.ts
@@ -4,129 +4,130 @@ import { PuzzlePart } from '../puzzles/base/puzzleBase';
 import { Puzzle2 } from '../puzzles/puzzle_2';
 import { Puzzle3 } from '../puzzles/puzzle_3';
 import { Puzzle4 } from '../puzzles/puzzle_4';
+import {Puzzle6} from "../puzzles/puzzle_6.ts";
 
-//
-// describe("Puzzle 1", () => {
-//     test("Get the total difference with test file", () => {
-//         const puzzle_1 = new Puzzle1(1);
-//
-//         const result = puzzle_1.runFromFile(__dirname + '/assets/test_1.txt', PuzzlePart.ONE);
-//
-//         expect(result.resultNumber).toBe(11);
-//     })
-//
-//     test("Get the total difference", () => {
-//         const puzzle_1 = new Puzzle1(1);
-//
-//         const result = puzzle_1.runFromFile(__dirname + '/../assets/input/1.txt', PuzzlePart.ONE);
-//
-//         expect(result.resultNumber).toBe(2192892);
-//     })
-//
-//     test("Get the the amount of occurances with the test file", () => {
-//         const puzzle_1 = new Puzzle1(1);
-//
-//         const result = puzzle_1.runFromFile(__dirname + '/assets/test_1.txt', PuzzlePart.TWO);
-//
-//         expect(result.resultNumber).toBe(31);
-//     })
-//
-//     test("Get the the amount of occurances", () => {
-//         const puzzle_1 = new Puzzle1(1);
-//
-//         const result = puzzle_1.runFromFile(__dirname + '/../assets/input/1.txt', PuzzlePart.TWO);
-//
-//         expect(result.resultNumber).toBe(22962826);
-//     })
-//
-// });
-//
-// describe("Puzle 2", () => {
-//     test("Get the amount of safe reports of test file", () => {
-//
-//         const puzzle_2 = new Puzzle2(2);
-//
-//         const result = puzzle_2.runFromFile(__dirname + '/assets/test_2.txt', PuzzlePart.ONE);
-//
-//         expect(result.resultNumber).toBe(2);
-//
-//     });
-//
-//     test("Get the amount of safe reports", () => {
-//
-//         const puzzle_2 = new Puzzle2(2);
-//
-//         const result = puzzle_2.runFromFile(__dirname + '/../assets/input/2.txt', PuzzlePart.ONE);
-//
-//         expect(result.resultNumber).toBe(230);
-//
-//     });
-//
-//     test("Get the amount of safe reports of test file", () => {
-//
-//         const puzzle_2 = new Puzzle2(2);
-//
-//         const result = puzzle_2.runFromFile(__dirname + '/assets/test_2.txt', PuzzlePart.TWO);
-//
-//         expect(result.resultNumber).toBe(4);
-//
-//     });
-//
-//     test("Get the amount of safe reports", () => {
-//
-//         const puzzle_2 = new Puzzle2(2);
-//
-//         const result = puzzle_2.runFromFile(__dirname + '/../assets/input/2.txt', PuzzlePart.TWO);
-//
-//         expect(result.resultNumber).toBe(301);
-//
-//     });
-// });
-//
-// describe("Puzle 3", () => {
-//
-//     test("Get the total of muls of test file", () => {
-//
-//         const puzzle_3 = new Puzzle3(3);
-//
-//         const result = puzzle_3.runFromFile(__dirname + '/assets/test_3.txt', PuzzlePart.ONE);
-//
-//         expect(result.resultNumber).toBe(161);
-//
-//     });
-//
-//     test("Get the total of muls", () => {
-//
-//         const puzzle_3 = new Puzzle3(3);
-//
-//         const result = puzzle_3.runFromFile(__dirname + '/../assets/input/3.txt', PuzzlePart.ONE);
-//
-//         expect(result.resultNumber).toBe(184511516);
-//
-//     });
-//
-//     test("Get the total of muls 2 of test file", () => {
-//
-//         const puzzle_3 = new Puzzle3(3);
-//
-//         const result = puzzle_3.runFromFile(__dirname + '/assets/test_3_B.txt', PuzzlePart.TWO);
-//
-//         expect(result.resultNumber).toBe(48);
-//
-//     });
-//
-//     test("Get the total of muls 2", () => {
-//
-//         const puzzle_3 = new Puzzle3(3);
-//
-//         const result = puzzle_3.runFromFile(__dirname + '/../assets/input/3.txt', PuzzlePart.TWO);
-//
-//         expect(result.resultNumber).toBe(90044227);
-//
-//     });
-//
-//
-// });
+
+describe("Puzzle 1", () => {
+    test("Get the total difference with test file", () => {
+        const puzzle_1 = new Puzzle1(1);
+
+        const result = puzzle_1.runFromFile(__dirname + '/assets/test_1.txt', PuzzlePart.ONE);
+
+        expect(result.resultNumber).toBe(11);
+    })
+
+    test("Get the total difference", () => {
+        const puzzle_1 = new Puzzle1(1);
+
+        const result = puzzle_1.runFromFile(__dirname + '/../assets/input/1.txt', PuzzlePart.ONE);
+
+        expect(result.resultNumber).toBe(2192892);
+    })
+
+    test("Get the the amount of occurances with the test file", () => {
+        const puzzle_1 = new Puzzle1(1);
+
+        const result = puzzle_1.runFromFile(__dirname + '/assets/test_1.txt', PuzzlePart.TWO);
+
+        expect(result.resultNumber).toBe(31);
+    })
+
+    test("Get the the amount of occurances", () => {
+        const puzzle_1 = new Puzzle1(1);
+
+        const result = puzzle_1.runFromFile(__dirname + '/../assets/input/1.txt', PuzzlePart.TWO);
+
+        expect(result.resultNumber).toBe(22962826);
+    })
+
+});
+
+describe("Puzle 2", () => {
+    test("Get the amount of safe reports of test file", () => {
+
+        const puzzle_2 = new Puzzle2(2);
+
+        const result = puzzle_2.runFromFile(__dirname + '/assets/test_2.txt', PuzzlePart.ONE);
+
+        expect(result.resultNumber).toBe(2);
+
+    });
+
+    test("Get the amount of safe reports", () => {
+
+        const puzzle_2 = new Puzzle2(2);
+
+        const result = puzzle_2.runFromFile(__dirname + '/../assets/input/2.txt', PuzzlePart.ONE);
+
+        expect(result.resultNumber).toBe(230);
+
+    });
+
+    test("Get the amount of safe reports of test file", () => {
+
+        const puzzle_2 = new Puzzle2(2);
+
+        const result = puzzle_2.runFromFile(__dirname + '/assets/test_2.txt', PuzzlePart.TWO);
+
+        expect(result.resultNumber).toBe(4);
+
+    });
+
+    test("Get the amount of safe reports", () => {
+
+        const puzzle_2 = new Puzzle2(2);
+
+        const result = puzzle_2.runFromFile(__dirname + '/../assets/input/2.txt', PuzzlePart.TWO);
+
+        expect(result.resultNumber).toBe(301);
+
+    });
+});
+
+describe("Puzle 3", () => {
+
+    test("Get the total of muls of test file", () => {
+
+        const puzzle_3 = new Puzzle3(3);
+
+        const result = puzzle_3.runFromFile(__dirname + '/assets/test_3.txt', PuzzlePart.ONE);
+
+        expect(result.resultNumber).toBe(161);
+
+    });
+
+    test("Get the total of muls", () => {
+
+        const puzzle_3 = new Puzzle3(3);
+
+        const result = puzzle_3.runFromFile(__dirname + '/../assets/input/3.txt', PuzzlePart.ONE);
+
+        expect(result.resultNumber).toBe(184511516);
+
+    });
+
+    test("Get the total of muls 2 of test file", () => {
+
+        const puzzle_3 = new Puzzle3(3);
+
+        const result = puzzle_3.runFromFile(__dirname + '/assets/test_3_B.txt', PuzzlePart.TWO);
+
+        expect(result.resultNumber).toBe(48);
+
+    });
+
+    test("Get the total of muls 2", () => {
+
+        const puzzle_3 = new Puzzle3(3);
+
+        const result = puzzle_3.runFromFile(__dirname + '/../assets/input/3.txt', PuzzlePart.TWO);
+
+        expect(result.resultNumber).toBe(90044227);
+
+    });
+
+
+});
 
 
 describe("Puzzle 4", () => {
@@ -166,4 +167,26 @@ describe("Puzzle 4", () => {
         expect(result.resultNumber).toBeGreaterThan(9);
 
     });
+})
+
+describe("Puzzle 6", () => {
+
+    test("Guard Gallivant with test file", () => {
+        const puzzle_6 = new Puzzle6(6);
+
+        const result = puzzle_6.runFromFile(__dirname + '/assets/test_6.txt', PuzzlePart.ONE);
+
+        expect(result.resultNumber).toBe(41);
+    })
+
+    test("Guard Gallivant", () => {
+        const puzzle_6 = new Puzzle6(6);
+
+        const result = puzzle_6.runFromFile(__dirname + '/../assets/input/6.txt', PuzzlePart.ONE);
+
+        expect(result.resultNumber).toBeGreaterThan(41);
+
+    });
+
+
 })
